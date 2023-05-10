ab64cc0b70f87506fd98116c8f03a6020e9f0332
Tanya <t.akberdina@gmail.com>
2023-05-10 17:22:31 +0500

    hw5 + игра

---
+++ script.js
@@ -0,0 +1,14 @@
+function season () {
+    let monthNumber = prompt ("Ведите номер месяца (от 1 до 12) и вы узнаете к какому времени года он относится");
+    if (monthNumber >= 12 || monthNumber <= 2){
+        alert ( "Зима");
+      } else if (monthNumber <= 5 && monthNumber >= 3) {
+        alert ( "Весна");
+      } else if (monthNumber <= 8 && monthNumber >= 6) {
+        alert ("Лето");
+      } else if (monthNumber <= 11 && monthNumber >=7){
+        alert ("Осень")
+      } else {
+        alert ("Попробуй еще раз. В году всего 12 месяцев")
+      }
+}
